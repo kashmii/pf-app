0387f39fdd2582d4d0ebfafa4a4eed33ff67be73
kashmii <www.myac@gmail.com>
2024-06-19 23:32:12 +0900

    adding css to header/footer

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import s from './Header.module.css'
 
 const Header: React.FC = () => {
   return (
     <header>
-      <h1>My Portfolio</h1>
-      <nav>
+      <div className={s.titleContainer}>
+        <h1 className={s.title}>Miki's Portfolio</h1>
+      </div>
+      <nav className={s.nav}>
         <a href="/">Home</a>
         <a href="/projects">Projects</a>
       </nav>
